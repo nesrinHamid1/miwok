d16a3e7e133c73dafe2fb34a005b6b840e81b6a1
Nesrin Almehmadi <nhalmehmadi@gmail.com>
2020-09-08 23:02:25 +0300

    adding id var for images in the class

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -5,10 +5,12 @@ package com.example.android.miwok;
 public class Word {
     private String mMiwokWord;
     private String mDefaultWord;
+    private int mid;
 
-    public Word (String miwokWord, String defaultWord){
+    public Word (String miwokWord, String defaultWord, int id){
         mMiwokWord = miwokWord;
         mDefaultWord = defaultWord;
+        mid = id;
     }
 
 
@@ -18,5 +20,9 @@ public class Word {
 
     public String getDefaultWord(){
         return mDefaultWord;
+    }
+
+    public int getId(){
+        return mid;
     }
 }
